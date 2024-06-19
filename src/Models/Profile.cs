f499137fec0f888579f3ea8ca869f611ab67ff29
Timberfang <timberfang.code@pm.me>
2024-06-19 16:06:44 -0500

    feat: Create basic Profile class

---
+++ src/Models/Profile.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace FurBuilder.Models
+{
+    public class Profile
+    {
+        // TODO: Check data types on these fields before using them.
+
+        private string Name;
+        private string Species;
+        private string DateOfBirth;
+        private string Sex;
+        private string Owner;
+        private string EyeColor;
+        private string[] FurColors;
+        private string Backstory;
+        private float Height;
+        private float Weight;
+        private string Build;
+        private string Type;
+        private Guid ID;
+        private string IssueDate;
+    }
+}
