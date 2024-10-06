ee5c1f77617567a4b3e0608b5be10a5ad073cffd
Timberfang <timberfang.code@pm.me>
2024-10-06 14:08:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FurBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
