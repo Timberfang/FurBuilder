4a1d7fdd808822719df4c5327376a78a704e7479
Timberfang <timberfang.code@pm.me>
2024-10-11 14:13:02 -0500

    feat: Implement ICharacter

---
+++ Data/Appearance.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FurBuilder.Data
+{
+    public class Appearance : IAppearance
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IDictionary<string, string> Colors { get; set; }
+        public string Build { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public IList<string> PhysicalFeatures { get; set; }
+
+        public Appearance(string Name, string Description = "", IDictionary<string, string>? Colors = null, string Build = "", float Height = 0, float Weight = 0, IList<string>? PhysicalFeatures = null)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Colors = Colors ?? new Dictionary<string, string>();
+            this.Build = Build;
+            this.Height = Height;
+            this.Weight = Weight;
+            this.PhysicalFeatures = PhysicalFeatures ?? [];
+        }
+    }
+}
---
+++ Data/Character.cs
@@ -0,0 +1,58 @@
+﻿namespace FurBuilder.Data
+{
+    public class Character : ICharacter
+    {
+        // Metadata
+        public Guid Id { get; }
+        public string Owner { get; set; }
+        public DateTime CreatedAt { get; }
+        public IList<string> Tags { get; set; }
+
+        // Basic information
+        public string ProfileImage { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        // Detailed information
+        public IList<IAppearance> Forms { get; set; }
+        public IList<string> Personality { get; set; }
+        public string Background { get; set; }
+        public string Notes { get; set; }
+
+        public Character(string Owner, string Name, string Species, IList<string>? Tags = null, string ProfileImage = "", string Gender = "", int Age = 0)
+        {
+            Id = new();
+            this.Owner = Owner;
+            CreatedAt = DateTime.Now;
+            this.Tags = Tags ?? [];
+
+            this.ProfileImage = ProfileImage;
+            this.Name = Name;
+            this.Species = Species;
+            this.Gender = Gender;
+            this.Age = Age;
+
+            Forms = [];
+            Personality = [];
+            Background = "";
+            Notes = "";
+        }
+
+        // Methods
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+        public string ToJson()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToMarkdown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
