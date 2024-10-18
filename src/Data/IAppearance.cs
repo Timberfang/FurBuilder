248c5f2fdb6cb4ff809ba0646764fcdc515c778b
Timberfang <timberfang.code@pm.me>
2024-10-18 15:30:25 -0500

    docs: Clarify IAppearance properties

--- src/Data/IAppearance.cs
+++ src/Data/IAppearance.cs
@@ -2,8 +2,8 @@
 {
     public interface IAppearance
     {
-        string Name { get; set; } // Name the form (e.g. "Werewolf Form", "Human Form", etc.)
-        string Description { get; set; } // Describe the form (e.g. "A tall, muscular wolf-like humanoid")
+        string Name { get; set; } // Name the form (e.g. "Werewolf Form", "Human Form", etc.); use "Base" if there is only one form.
+        string Description { get; set; } // Describe the appearance in this form (e.g. "A tall, muscular wolf-like humanoid")
         IDictionary<string, string> Colors { get; set; } // Key for body part (eyes, fur, hair, skin, etc.), value for color.
         string Build { get; set; }
         float Height { get; set; } // In centimeters
