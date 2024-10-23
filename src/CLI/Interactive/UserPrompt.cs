654d6313f0133a0d2f3fc47ec0865627d4a00548
Timberfang <timberfang.code@pm.me>
2024-10-23 16:36:15 -0500

    feat: Add List creator method

--- src/CLI/Interactive/UserPrompt.cs
+++ src/CLI/Interactive/UserPrompt.cs
@@ -8,5 +8,32 @@ namespace FurBuilder.CLI.Interactive
 		{
 			return AnsiConsole.Ask<Type>($"Enter your character's {TraitName}:");
 		}
+
+		internal static IList<string> GetTraitList (string TraitName, bool DisplayList = false)
+		{
+			List<string> Output = [];
+			while (true)
+			{
+				Console.Clear();
+				if (DisplayList)
+				{
+					AnsiConsole.WriteLine($"{TraitName} List:");
+					if (Output.Count == 0) { AnsiConsole.WriteLine("- (Empty)"); }
+					else
+					{
+						foreach (string ListItem in Output)
+						{
+							AnsiConsole.WriteLine($"- {ListItem}");
+						}
+					}
+					AnsiConsole.WriteLine();
+				}
+
+				string UserInput = AnsiConsole.Ask<string>($"Enter a trait of type '{TraitName}', or type 'exit' to exit:");
+				if (!UserInput.Equals("exit", StringComparison.OrdinalIgnoreCase)) { Output.Add(UserInput); }
+				else { break; }
+			}
+			return Output;
+		}
 	}
 }
