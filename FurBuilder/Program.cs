ebae60647f756e913e238ce186063cf062981bc8
Timberfang <timberfang.code@pm.me>
2024-11-22 14:47:32 -0600

    feat: Start groundwork for frontend

---
+++ FurBuilder/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace FurBuilder;
+
+internal static class Program
+{
+	private static void Main(string[] args) => Views.ConsoleView.MainMenu.Start();
+}
---
+++ FurBuilder/Views/ConsoleView/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿namespace FurBuilder.Views.ConsoleView;
+
+internal static class MainMenu
+{
+	internal static void Start()
+	{
+		Console.WriteLine("Welcome to FurBuilder!");
+		Console.WriteLine();
+		Console.WriteLine("Let's make a character.");
+		Console.ReadLine();
+	}
+}
