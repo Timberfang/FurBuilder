766f8e6ffff63528e8e735a20a6dcfbf9710ae7d
Timberfang <timberfang.code@pm.me>
2024-11-24 18:59:46 -0600

    feat: Add basic character creation TUI

--- FurBuilder/Views/ConsoleView/MainMenu.cs
+++ FurBuilder/Views/ConsoleView/MainMenu.cs
@@ -1,4 +1,8 @@
-﻿namespace FurBuilder.Views.ConsoleView;
+﻿using FurBuilder.Models.Character;
+
+using Spectre.Console;
+
+namespace FurBuilder.Views.ConsoleView;
 
 internal static class MainMenu
 {
@@ -8,5 +12,16 @@ internal static class MainMenu
 		Console.WriteLine();
 		Console.WriteLine("Let's make a character.");
 		Console.ReadLine();
+
+		BasicAttributes characterAttributes = new(
+			AnsiConsole.Ask<string>("Character name:"),
+			AnsiConsole.Ask<string>("Character species:"),
+			AnsiConsole.Ask<string>("Character gender:"),
+			AnsiConsole.Ask<int>("Character age:")
+		);
+
+		Character currentCharacter = new(basicAttributes: characterAttributes);
+		Console.WriteLine("Here's your character so far:");
+		Console.Write(currentCharacter.ToString());
 	}
 }
