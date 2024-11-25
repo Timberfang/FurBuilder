e9328aeec542cd5e2972c3badb4a09bc64078d62
Timberfang <timberfang.code@pm.me>
2024-11-25 13:28:03 -0600

    fix: Make IMentalAttributes internal

--- FurBuilder/Models/Character/IMentalAttributes.cs
+++ FurBuilder/Models/Character/IMentalAttributes.cs
@@ -2,10 +2,10 @@ namespace FurBuilder.Models.Character;
 
 internal interface IMentalAttributes
 {
-	IList<string> Traits { get; set; }
-	IList<string> Likes { get; set; }
-	IList<string> Dislikes { get; set; }
-	string Backstory { get; set; }
+	internal IList<string> Traits { get; set; }
+	internal IList<string> Likes { get; set; }
+	internal IList<string> Dislikes { get; set; }
+	internal string Backstory { get; set; }
 
 	internal string ToString();
 }
