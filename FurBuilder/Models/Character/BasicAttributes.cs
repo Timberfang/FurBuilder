86ae20f75df141def183ee23a8c1df8c3e917f9c
Timberfang <timberfang.code@pm.me>
2024-11-25 09:29:46 -0600

    fix: Check for negatives when setting age

--- FurBuilder/Models/Character/BasicAttributes.cs
+++ FurBuilder/Models/Character/BasicAttributes.cs
@@ -2,11 +2,16 @@ namespace FurBuilder.Models.Character;
 
 public class BasicAttributes(string name = "", string species = "", string gender = "", int age = 0) : IBasicAttributes
 {
+	private int _age = age;
+
 	public string Name { get; set; } = name;
 	public string Species { get; set; } = species;
 	public string Gender { get; set; } = gender;
-	public int Age { get; set; } = age;
-
+	public int Age
+	{
+		get => _age;
+		set => _age = value >= 0 ? value : throw new ArgumentOutOfRangeException(nameof(Age), "Age cannot be negative.");
+	}
 	public override string ToString()
 	{
 		return $"{Name} ({Species}, {Gender}, {Age})";
